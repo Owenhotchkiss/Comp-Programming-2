d8a66663411b848038a3b34de465cc96f22d835d
Owenhotchkiss <113038190+Owenhotchkiss@users.noreply.github.com>
2024-01-24 14:38:20 -0600

    1/24

---
+++ prog54b/prog54b/MainForm.py
@@ -0,0 +1,166 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._label1 = System.Windows.Forms.Label()
+		self._label2 = System.Windows.Forms.Label()
+		self._label3 = System.Windows.Forms.Label()
+		self._label4 = System.Windows.Forms.Label()
+		self._label5 = System.Windows.Forms.Label()
+		self._label6 = System.Windows.Forms.Label()
+		self._textBox1 = System.Windows.Forms.TextBox()
+		self._textBox2 = System.Windows.Forms.TextBox()
+		self._textBox3 = System.Windows.Forms.TextBox()
+		self._textBox4 = System.Windows.Forms.TextBox()
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# label1
+		# 
+		self._label1.BackColor = System.Drawing.Color.IndianRed
+		self._label1.Location = System.Drawing.Point(12, 35)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(100, 23)
+		self._label1.TabIndex = 0
+		self._label1.Text = "Number 1"
+		# 
+		# label2
+		# 
+		self._label2.BackColor = System.Drawing.Color.IndianRed
+		self._label2.Location = System.Drawing.Point(12, 68)
+		self._label2.Name = "label2"
+		self._label2.Size = System.Drawing.Size(100, 23)
+		self._label2.TabIndex = 1
+		self._label2.Text = "Number 2"
+		# 
+		# label3
+		# 
+		self._label3.BackColor = System.Drawing.Color.IndianRed
+		self._label3.Location = System.Drawing.Point(12, 104)
+		self._label3.Name = "label3"
+		self._label3.Size = System.Drawing.Size(100, 23)
+		self._label3.TabIndex = 2
+		self._label3.Text = "Number 3"
+		# 
+		# label4
+		# 
+		self._label4.BackColor = System.Drawing.Color.IndianRed
+		self._label4.Location = System.Drawing.Point(12, 134)
+		self._label4.Name = "label4"
+		self._label4.Size = System.Drawing.Size(100, 23)
+		self._label4.TabIndex = 3
+		self._label4.Text = "Number 4"
+		# 
+		# label5
+		# 
+		self._label5.BackColor = System.Drawing.Color.IndianRed
+		self._label5.Location = System.Drawing.Point(12, 231)
+		self._label5.Name = "label5"
+		self._label5.Size = System.Drawing.Size(100, 23)
+		self._label5.TabIndex = 4
+		self._label5.Text = "Sum:"
+		# 
+		# label6
+		# 
+		self._label6.BackColor = System.Drawing.Color.IndianRed
+		self._label6.Location = System.Drawing.Point(12, 272)
+		self._label6.Name = "label6"
+		self._label6.Size = System.Drawing.Size(100, 23)
+		self._label6.TabIndex = 5
+		self._label6.Text = "Average:"
+		# 
+		# textBox1
+		# 
+		self._textBox1.Location = System.Drawing.Point(119, 35)
+		self._textBox1.Name = "textBox1"
+		self._textBox1.Size = System.Drawing.Size(100, 20)
+		self._textBox1.TabIndex = 6
+		# 
+		# textBox2
+		# 
+		self._textBox2.Location = System.Drawing.Point(119, 70)
+		self._textBox2.Name = "textBox2"
+		self._textBox2.Size = System.Drawing.Size(100, 20)
+		self._textBox2.TabIndex = 7
+		# 
+		# textBox3
+		# 
+		self._textBox3.Location = System.Drawing.Point(119, 104)
+		self._textBox3.Name = "textBox3"
+		self._textBox3.Size = System.Drawing.Size(100, 20)
+		self._textBox3.TabIndex = 8
+		# 
+		# textBox4
+		# 
+		self._textBox4.Location = System.Drawing.Point(119, 136)
+		self._textBox4.Name = "textBox4"
+		self._textBox4.Size = System.Drawing.Size(100, 20)
+		self._textBox4.TabIndex = 9
+		# 
+		# button1
+		# 
+		self._button1.Location = System.Drawing.Point(191, 213)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(89, 47)
+		self._button1.TabIndex = 10
+		self._button1.Text = "Calculate"
+		self._button1.UseVisualStyleBackColor = True
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.Location = System.Drawing.Point(286, 213)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(90, 49)
+		self._button2.TabIndex = 11
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = True
+		# 
+		# button3
+		# 
+		self._button3.Location = System.Drawing.Point(191, 268)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(185, 45)
+		self._button3.TabIndex = 12
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = True
+		# 
+		# MainForm
+		# 
+		self.BackColor = System.Drawing.Color.Coral
+		self.ClientSize = System.Drawing.Size(465, 317)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Controls.Add(self._textBox4)
+		self.Controls.Add(self._textBox3)
+		self.Controls.Add(self._textBox2)
+		self.Controls.Add(self._textBox1)
+		self.Controls.Add(self._label6)
+		self.Controls.Add(self._label5)
+		self.Controls.Add(self._label4)
+		self.Controls.Add(self._label3)
+		self.Controls.Add(self._label2)
+		self.Controls.Add(self._label1)
+		self.Name = "MainForm"
+		self.Text = "prog54b"
+		self.ResumeLayout(False)
+		self.PerformLayout()
+
+
+	def Button1Click(self, sender, e):
+		num1 = int(self._textBox1.Text)
+		num2 = int(self._textBox2.Text)
+		num3 = int(self._textBox3.Text)
+		num4 = int(self._textBox4.Text)
+		sum = num1 + num2 + num3 + num4
+		average = sum / 4
