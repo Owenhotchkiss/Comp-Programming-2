8b43dc5ecf95a45642083167c91f79db8a8eadfd
Owenhotchkiss <113038190+Owenhotchkiss@users.noreply.github.com>
2024-02-01 20:14:21 +0000

    1/2

--- lp4-9.py
+++ lp4-9.py
@@ -1,5 +1,8 @@
 import random
 rnd = random.randint(1, 20)
 num = int(input("Enter a number between 1 and 20: "))
+print(rnd)
 if rnd == num:
   print("You Won!!")
+else:
+  print("Better Luck Next Time!!")
