6837d4ccc9a0351683462b7413b761be6d73b15d
Owenhotchkiss <113038190+Owenhotchkiss@users.noreply.github.com>
2024-04-23 14:36:23 -0500

    4/23

---
+++ Csharp/CSharp/LP4-2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter Weight: ");
+int weight = int.Parse(Console.ReadLine());
+
+Console.Write("Enter length: ");
+int length = int.Parse(Console.ReadLine());
+
+Console.Write("Enter width: ");
+int width = int.Parse(Console.ReadLine());
+
+Console.Write("Enter height: ");
+int height = int.Parse(Console.ReadLine());
+
+int volume = length * width * height;
+
+if (weight > 27 && volume > 100_000);
+    Console.WriteLine("Package is too heavy or too large");
+else if (weight > 27)
+    Console.WriteLine("Package is too heavy");
+else if (volume > 100_000)
+    Console.WriteLine("Package is okay");
+
+Console.ReadLine();
