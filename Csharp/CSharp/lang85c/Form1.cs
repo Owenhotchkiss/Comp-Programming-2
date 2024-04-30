7ec7a5643ffbd2903c58e53f4e2e6046c109781c
Owenhotchkiss <113038190+Owenhotchkiss@users.noreply.github.com>
2024-04-30 14:34:38 -0500

    4/30

---
+++ Csharp/CSharp/lang85c/Form1.cs
@@ -0,0 +1,10 @@
+namespace lang85c
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Csharp/CSharp/pg347Sum/Form1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualBasic;
+namespace pg347Sum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string variable = Interaction.InputBox("Prompt here", "Title");
+            MessageBox.Show(variable);
+        }
+    }
+}
